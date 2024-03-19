8490d984f1a7244d5ed84b4d2b0bd07ee6ec7d46
Gideon <keterjr1@gmail.com>
2024-03-19 22:06:21 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideFeet){
+  if(rideFeet > 2500){ 
+    scuberGreetingForFeet='No can do.'
+    return scuberGreetingForFeet
+  }
+
+  else if(rideFeet <= 400){
+    scuberGreetingForFeet='This one is on me!'
+    return scuberGreetingForFeet
+  }
+
+  else if(rideFeet > 2000){
+    scuberGreetingForFeet='I will gladly take your thirty bucks.'
+    return scuberGreetingForFeet
+  }
+
+  else if(rideFeet > 400 <= 2000){
+    scuberGreetingForFeet='That will be twenty bucks.'
+    return scuberGreetingForFeet
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":  
+      return "Thank you."
+    default:
+      return "Bye.";
+  }
 }
